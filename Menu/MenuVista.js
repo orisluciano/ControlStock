d05b2e00d59bebdcde5cc5d0742f3c9a1ed4e9d0
Luciano <orisluciano@gmail.com>
2026-05-04 12:13:39 -0300

    producto vista html y js

--- Menu/MenuVista.js
+++ Menu/MenuVista.js
@@ -1,4 +1,5 @@
 import Inicio from "../Inicio/Inicio.js";
+import ProductoVista from "../Producto/Vista/ProductoVista.js";
 import Formulario from "../Utiles/Formulario.js";
 import Identificadores from "../Utiles/Identificadores.js";
 
@@ -30,55 +31,15 @@ class MenuVista {
         div.className = "divMenu dropdown";
         let root = document.getElementById(this.ident.root);
         root.innerHTML = "";
-        //this.cargarFunciones();
+        this.cargarFunciones();
     }
 
     cargarFunciones(){
         let esto = this;
-        let btnConfig = document.getElementById(this.ids.btnConfig);
-        btnConfig.onclick = function() {
-            esto.btnConfigOnClick();           
-        };
-        let btnLogout = document.getElementById(this.ids.btnLogout);
-        btnLogout.onclick = function() {
-            esto.btnLogoutOnClick();
-        };
-        let btnPerfil = document.getElementById(this.ids.btnPerfil);
-        btnPerfil.onclick = function(params) {
-            esto.btnPerfilOnClick();
-        };
-        let btnUsers = document.getElementById(this.ids.btnUsers);
-        btnUsers.onclick = function(params) {
-            esto.btnUsersOnClick();
-        };
-        let btnRubro = document.getElementById(this.ids.btnRubro);
-        btnRubro.onclick = function(params) {
-            esto.btnRubroOnClick();
-        };
-        let btnSugerencias = document.getElementById(this.ids.btnSugerencias);
-        btnSugerencias.onclick = function(params) {
-            esto.btnSugerenciasOnClick();
-        };
-        let btnTipoContacto = document.getElementById(this.ids.btnTipoContacto);
-        btnTipoContacto.onclick = function(params) {
-            esto.btnTipoContactoOnClick();
-        };
-        let btnTrabajador = document.getElementById(this.ids.btnTrabajador);
-        btnTrabajador.onclick = function(params) {
-            esto.btnTrabajadorOnClick();
-        };
-        let btnTrabContacto = document.getElementById(this.ids.btnTrabContacto);
-        btnTrabContacto.onclick = function(params) {
-            esto.btnTrabContactoOnClick();
-        };
-        let btnTrabOpi = document.getElementById(this.ids.btnTrabOpi);
-        btnTrabOpi.onclick = function(params) {
-            esto.btnTrabOpiOnClick();
-        };
-        let btnTrabRubro = document.getElementById(this.ids.btnRubro);
-        btnTrabRubro.onclick = function(params) {
-            esto.btnTrabRubroOnClick();
-        };
+        let btnProducto = document.getElementById(this.ids.btnProducto);
+        btnProducto.onclick = function() {
+            esto.btnProductoOnClick();
+        }
     }
 
     btnPerfilOnClick(){
@@ -127,6 +88,11 @@ class MenuVista {
         loginService.logout();
         let inicio = new Inicio();
         inicio.iniciarApp();
+    }
+
+    btnProductoOnClick(){
+        let prodVista = new ProductoVista();
+        prodVista.cargarVista();
     }
 }
 
